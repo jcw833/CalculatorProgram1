d6e79217bb4df975a154b15b6db9f5bc807a729b
John Wahlstedt <johnwahlstedt@Johns-MacBook-Pro-2.local>
2019-09-11 12:17:57 -0400

    Interface for calculator

---
+++ interface.py
@@ -0,0 +1,11 @@
+def interface():
+    keep_running = True
+    while keep_running:
+        print("My Program")
+        print("Options:")
+        print("9 - Quit")
+        choice = input("Enter your choice: ")
+        if choice=='9':
+            keep_running = False
+    return
+   
