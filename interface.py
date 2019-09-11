1b406e4afdf5698e0596936cb3e513ec2592b26c
John Wahlstedt <johnwahlstedt@Johns-MacBook-Pro-2.local>
2019-09-11 12:37:01 -0400

    added final changes

--- interface.py
+++ interface.py
@@ -3,9 +3,11 @@ def interface():
     while keep_running:
         print("My Program")
         print("Options:")
+        print("1 - Cholesterol Check")
         print("9 - Quit")
         choice = input("Enter your choice: ")
         if choice=='9':
             keep_running = False
+        elif choice == '1':
+            cholesterol_check()
     return
-   
